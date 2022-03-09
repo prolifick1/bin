fb2e4e6890fef3a79966369d896128dc45b3f394
Luis Manzo <manzosk3@gmail.com>
2022-03-09 12:06:28 -0600

    adjust to local time instead of UTC

--- brightness/brightness_adjust.py
+++ brightness/brightness_adjust.py
@@ -7,8 +7,8 @@ long = -89.81
 
 sun = Sun(lat, long)
 
-today_sr = sun.get_sunrise_time().strftime('%h:%M:%S')
-today_ss = sun.get_sunset_time().strftime('%H:%M:%S')
+today_sr = sun.get_local_sunrise_time().strftime('%H:%M:%S')
+today_ss = sun.get_local_sunset_time().strftime('%H:%M:%S')
 
 now = datetime.now()
 
@@ -16,8 +16,10 @@ current_time = now.strftime('%H:%M:%S')
 
 #if between dusk and sunset, brightness 1
 if(current_time > today_sr and current_time < today_ss):
+    print(f"current time: {current_time} sr: {today_sr} ss: {today_ss}")
     subprocess.check_call(['/usr/local/bin/brightness', '-m', '1'])
 
 #if it's between sunset and dusk, brightness 0.05
-if(current_time > today_ss or current_time < today_sr):
+if((current_time > today_ss) or (current_time < today_sr)):
+    print(f"current time: {current_time} sr: {today_sr} ss: {today_ss}")
     subprocess.check_call(['/usr/local/bin/brightness', '-m', '0.05'])  
